a8952628bf84a4cac607ecbd4ba6e5d3f1a19adb
eperezvazquez <perezelisa21@gmail.com>
2022-07-25 21:39:21 +0000

    utilitis.v1

---
+++ src/textpreprocessing.py
@@ -0,0 +1,15 @@
+#Recibo un string mas un diccionario y devulevo un string
+#Funcion esta creada para dividir texto .
+def clean_stopwords(text: str,stop_dict: dict)->str:
+    if text is not None:
+        words = text.split()
+        words_clean = []
+        for word in words:
+            if word not in stop_dict:
+                words_clean.append(word)
+        result = ' '.join(words_clean)
+    else:
+        result = None
+    return result
+
+
