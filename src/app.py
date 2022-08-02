1dcc1b33883e742c68c0195ba027a7cbe541f9b7
eperezvazquez <perezelisa21@gmail.com>
2022-08-02 13:28:27 +0000

    NLP SpamOK

--- src/app.py
+++ src/app.py
@@ -1,4 +1,128 @@
-from utils import db_connect
-engine = db_connect()
+# importo librerias
+import pandas as pd
+import pickle
+import numpy as np
+import re
+import unicodedata
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+import pickle
 
-# your code here
+df_raw = pd.read_csv('https://raw.githubusercontent.com/4GeeksAcademy/NLP-project-tutorial/main/url_spam.csv')
+
+df_raw['is_spam'].value_counts()
+
+df = df_raw.copy()
+
+df = df.drop_duplicates().reset_index(drop = True)
+
+# varias funciones
+
+def comas(text):
+    """
+    Elimina comas del texto
+    """
+    return re.sub(',', ' ', text)
+
+def espacios(text):
+    """
+    Elimina enters dobles por un solo enter
+    """
+    return re.sub(r'(\n{2,})','\n', text)
+
+def minuscula(text):
+    """
+    Cambia mayusculas a minusculas
+    """
+    return text.lower()
+
+def numeros(text):
+    """
+    Sustituye los numeros
+    """
+    return re.sub('([\d]+)', ' ', text)
+
+def caracteres_no_alfanumericos(text):
+    """
+    Sustituye caracteres raros, no digitos y letras
+    Ej. hola 'pepito' como le va? -> hola pepito como le va
+    """
+    return re.sub("(\\W)+"," ",text)
+
+def comillas(text):
+    """
+    Sustituye comillas por un espacio
+    Ej. hola 'pepito' como le va? -> hola pepito como le va?
+    """
+    return re.sub("'"," ", text)
+
+def palabras_repetidas(text):
+    """
+    Sustituye palabras repetidas
+
+    Ej. hola hola, como les va? a a ustedes -> hola, como les va? a ustedes
+    """
+    return re.sub(r'\b(\w+)( \1\b)+', r'\1', text)
+
+def esp_multiple(text):
+    """
+    Sustituye los espacios dobles entre palabras
+    """
+    return re.sub(' +', ' ',text)
+
+'/'
+'.'
+'  '
+# funcón para eliminar https
+def url(text):
+    return re.sub(r'(https://www|https://)', '', text)
+
+# se limpia url
+df['url_limpia'] = df['url'].apply(url).apply(caracteres_no_alfanumericos).apply(esp_multiple)
+
+df['is_spam'] = df['is_spam'].apply(lambda x: 1 if x == True else 0)
+
+df['len_url'] = df['url'].apply(lambda x : len(x))
+df['contains_subscribe'] = df['url'].apply(lambda x : 1 if "subscribe" in x else 0)
+df['contains_hash'] = df['url'].apply(lambda x : 1 if "#" in x else 0)
+df['num_digits'] = df['url'].apply(lambda x : len("".join(_ for _ in x if _.isdigit())) )
+df['non_https'] = df['url'].apply(lambda x : 1 if "https" in x else 0)
+df['num_words'] = df['url'].apply(lambda x : len(x.split("/")))
+
+#Step 2
+target = 'is_spam'
+features = [f for f in df.columns if f not in ["url", target]]
+vec = CountVectorizer().fit_transform(df['url_limpia'])
+
+
+X_train, X_test, y_train, y_test = train_test_split(vec, df['is_spam'], stratify = df['is_spam'], random_state = 2207)
+
+#Step 3
+
+classifier = SVC(C = 1.0, kernel = 'linear', gamma = 'auto')
+
+classifier.fit(X_train, y_train)
+predictions = classifier.predict(X_test)
+print(classification_report(y_test, predictions))
+
+# optimizo hiperparámetros
+param_grid = {'C': [0.1,1, 10, 100], 'gamma': [1,0.1,0.01,0.001],'kernel': ['rbf', 'poly', 'sigmoid']}
+
+grid = GridSearchCV(SVC(random_state=1234),param_grid,verbose=2)
+grid.fit(X_train,y_train)
+
+grid.best_params_
+
+grid.best_estimator_
+
+predictions = grid.best_estimator_.predict(X_test)
+print(classification_report(y_test, predictions))
+
+#Step 4
+
+# se guarda el modelo
+filename = '../models/nb_model.sav'
+pickle.dump(classification_report, open(filename,'wb'))
